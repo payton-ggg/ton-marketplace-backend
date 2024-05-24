370243e804b8707d96831c0ccb7520d3feb1cc06
Platon <111748152+payton-ggg@users.noreply.github.com>
2024-05-24 22:51:20 +0300

    Create balanceSec.js

---
+++ server/helpers/balanceSec.js
@@ -0,0 +1,38 @@
+export const calculateBalance = (incomeData, user) => {
+  const AMOUNT = 30;
+  let userBalance = user.balanceSec;
+
+  const increase = () => {
+    userBalance += AMOUNT;
+  };
+
+  const increaseSec = () => {
+    userBalance += 40;
+  };
+
+  if (
+    incomeData.isTelegramComplete &&
+    incomeData.isTelegramComplete !== user.isTelegramComplete
+  ) {
+    increaseSec();
+  } else if (
+    incomeData.isXComplete &&
+    incomeData.isXComplete !== user.isXComplete
+  ) {
+    increase();
+  } else if (
+    incomeData.isMessComplete &&
+    incomeData.isMessComplete !== user.isMessComplete
+  ) {
+    increase();
+  } else if (
+    incomeData.isWalComplete &&
+    incomeData.isWalComplete !== user.isWalComplete
+  ) {
+    increase();
+  } else {
+    return userBalance;
+  }
+
+  return userBalance;
+};
