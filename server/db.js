5a78f4673275dc9c6f80a66f9864eb3583f5914a
Platon <111748152+payton-ggg@users.noreply.github.com>
2024-05-24 22:50:06 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,6 +3,7 @@ import { Schema, model } from "mongoose";
 const userSchema = new Schema({
   username: { type: String, required: true },
   balance: { type: Number, required: true },
+  balanceSec: { type: Number, required: true },
   isTelegramComplete: { type: Boolean, require: true},
   isXComplete: { type: Boolean, require: true},
   isMessComplete: { type: Boolean, require: true},
